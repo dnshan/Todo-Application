8cb23768e7aa3472df4f1262c1ac467d1e8e56c3
dnshan <dnshan46@gmail.com>
2024-05-11 13:19:56 +0530

    #04 Added NewTask file

---
+++ app/src/main/java/com/example/todo/AddNewTask.kt
@@ -0,0 +1,90 @@
+package com.example.todo
+
+import android.app.Activity
+import android.content.DialogInterface
+import android.graphics.Color
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import com.example.todo.Model.ToDoModel
+import com.example.todo.Utils.DataBaseHelper
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class AddNewTask : BottomSheetDialogFragment() {
+    //widgets
+    private var mEditText: EditText? = null
+    private var mSaveButton: Button? = null
+    private var myDb: DataBaseHelper? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.add_newtask, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mEditText = view.findViewById(R.id.edittext)
+        mSaveButton = view.findViewById(R.id.button_save)
+        myDb = DataBaseHelper(activity)
+        var isUpdate = false
+        val bundle = arguments
+        if (bundle != null) {
+            isUpdate = true
+            val task = bundle.getString("task")
+            mEditText.setText(task)
+            if (task!!.length > 0) {
+                mSaveButton.setEnabled(false)
+            }
+        }
+        mEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString() == "") {
+                    mSaveButton.setEnabled(false)
+                    mSaveButton.setBackgroundColor(Color.GRAY)
+                } else {
+                    mSaveButton.setEnabled(true)
+                    mSaveButton.setBackgroundColor(resources.getColor(R.color.colorPrimary))
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+        val finalIsUpdate = isUpdate
+        mSaveButton.setOnClickListener(View.OnClickListener {
+            val text = mEditText.getText().toString()
+            if (finalIsUpdate) {
+                myDb!!.updateTask(bundle!!.getInt("id"), text)
+            } else {
+                val item = ToDoModel()
+                item.task = text
+                item.status = 0
+                myDb!!.insertTask(item)
+            }
+            dismiss()
+        })
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        val activity: Activity? = activity
+        if (activity is OnDialogCloseListner) {
+            (activity as OnDialogCloseListner?).onDialogClose(dialog)
+        }
+    }
+
+    companion object {
+        const val TAG = "AddNewTask"
+        fun newInstance(): AddNewTask {
+            return AddNewTask()
+        }
+    }
+}
--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.todo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todo.Model.ToDoModel
+import com.example.todo.Utils.DataBaseHelper
+import com.example.todo.Adapter.ToDoAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.Collections
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnDialogCloseListner {
+    private var mRecyclerview: RecyclerView? = null
+    private var fab: FloatingActionButton? = null
+    private var myDB: DataBaseHelper? = null
+    private var mList: List<ToDoModel?>? = null
+    private var adapter: ToDoAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mRecyclerview = findViewById(R.id.recyclerview)
+        fab = findViewById(R.id.fab)
+        myDB = DataBaseHelper(this@MainActivity)
+        mList = ArrayList()
+        adapter = ToDoAdapter(myDB, this@MainActivity)
+        mRecyclerview.setHasFixedSize(true)
+        mRecyclerview.setLayoutManager(LinearLayoutManager(this))
+        mRecyclerview.setAdapter(adapter)
+        mList = myDB!!.allTasks
+        Collections.reverse(mList)
+        adapter.setTasks(mList)
+        fab.setOnClickListener(View.OnClickListener {
+            AddNewTask.newInstance().show(supportFragmentManager, AddNewTask.TAG)
+        })
+        val itemTouchHelper = ItemTouchHelper(RecyclerViewTouchHelper(adapter))
+        itemTouchHelper.attachToRecyclerView(mRecyclerview)
+    }
+
+    fun onDialogClose(dialogInterface: DialogInterface?) {
+        mList = myDB!!.allTasks
+        Collections.reverse(mList)
+        adapter.setTasks(mList)
+        adapter.notifyDataSetChanged()
     }
 }
